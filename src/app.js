203e6d0af05d1ea8cb1523b283f1d0229b3e4b1d
RUTH RODRIGUEZ <al25120225.utcj.edu.mx>
2026-02-23 17:43:58 -0700

    extructura inicial del proyecto

---
+++ src/app.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("btn");
+const msg = document.getElementById("msg");
+
+btn.addEventListener("click", () => {
+  msg.textContent = "JS funcionando y repo listo 🚀";
+});
